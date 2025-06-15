2ba082f51c4836f232f509feaba28844c00a3872
DESKTOP-04NRI2O\82104 <hyunook4929@gmail.com>
2025-06-15 19:52:15 +0900

    feat: Setup app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+
 function App() {
-  return <div>asdf</div>;
+  return (
+    <AppContainer>
+      <HeaderSection>Header</HeaderSection>
+
+      <ProfileSection>Profile</ProfileSection>
+
+      <SubscribeSection>Subscribe with full bleed image</SubscribeSection>
+
+      <CardSection>Cards</CardSection>
+    </AppContainer>
+  );
 }
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1680px;
+  width: 100%;
+  flex-grow: 0;
+  padding: 0 0 80px;
+  background-color: #fff;
+  margin: 0 auto;
+  border: 10px solid black;
+  min-height: 100vh;
+`;
+
+const HeaderSection = styled.section`
+  background-color: lightblue;
+`;
+
+const ProfileSection = styled.section`
+  background-color: lightgreen;
+`;
+
+const SubscribeSection = styled.section`
+  background-color: lightcoral;
+`;
+
+const CardSection = styled.section`
+  background-color: lightyellow;
+`;
 
 export default App;
